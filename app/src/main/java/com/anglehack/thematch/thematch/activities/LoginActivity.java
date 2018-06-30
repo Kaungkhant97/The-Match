4ae8322324204b2f7cad1017b153b23d844a623e
NyiNyiLin <nyiynilin.dev@gmail.com>
2018-06-30 15:31:31 +0630

    add log in fake screen

---
+++ app/src/main/java/com/anglehack/thematch/thematch/activities/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.anglehack.thematch.thematch.activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.anglehack.thematch.thematch.Manager.ChallengeManager;
+import com.anglehack.thematch.thematch.R;
+
+/**
+ * Created by IN-3442 on 30-Jun-18.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+        ChallengeManager challengeManager;
+
+    }
+
+}
